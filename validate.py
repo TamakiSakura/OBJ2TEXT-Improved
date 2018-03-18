205dd9acafd4e87c071359fa8003ceefc9cba37f
Wen Xiao <wendyxiao0609@gmail.com>
2018-03-18 13:29:25 -0700

    minor changes in train, and new validation file
    just run
    python3 validate.py
    to get the validation result of current model (default: 1 epoch)

--- validate.py
+++ validate.py
@@ -19,6 +19,15 @@ def to_var(x, volatile=False):
         x = x.cuda()
     return Variable(x, volatile=volatile)
 
+def load_image(image_path, transform=None):
+    image = Image.open(image_path)
+    image = image.resize([224, 224], Image.LANCZOS)
+    
+    if transform is not None:
+        image = transform(image).unsqueeze(0)
+    
+    return image
+
 def compute_bleu(reference_sentence, predicted_sentence):
     """
     Given a reference sentence, and a predicted sentence, compute the BLEU similary between them.
@@ -27,7 +36,7 @@ def compute_bleu(reference_sentence, predicted_sentence):
     predicted_tokenized = word_tokenize(predicted_sentence.lower())
     return sentence_bleu([reference_tokenized], predicted_tokenized)
 
-def validation(encoder,layout_encoder,decoder, args,vocab,transform, batch_size):
+def validation(layout_encoder,decoder, args,vocab,transform, batch_size,encoder=None):
     # Build data loader
     data_loader_val = get_loader(args.image_dir_val, args.caption_path_val, vocab, args.coco_detection_result_val,
                              transform, batch_size,
@@ -78,114 +87,51 @@ def validation(encoder,layout_encoder,decoder, args,vocab,transform, batch_size)
     print("Total number of Validation Images : %d, overall avg bleu: %f"%(i,bleu_score_all/(i*batch_size)))
 
 
-
-
-
-
-
 def main(args):
-
-    torch.manual_seed(args.seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(args.seed)
-
-    # Create model directory
-    if not os.path.exists(args.model_path):
-        os.makedirs(args.model_path)
-
-    # Image preprocessing
-    # For normalization, see https://github.com/pytorch/vision#models
+	# Image preprocessing
     transform = transforms.Compose([
-        # transforms.RandomCrop(args.crop_size),
-        # transforms.RandomHorizontalFlip(),
-        transforms.Scale(args.crop_size),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406),
+        transforms.ToTensor(), 
+        transforms.Normalize((0.485, 0.456, 0.406), 
                              (0.229, 0.224, 0.225))])
-
-    # Load vocabulary wrapper.
+    
+    # Load vocabulary wrapper
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
 
-    # Build data loader
-    data_loader = get_loader(args.image_dir, args.caption_path, vocab, args.coco_detection_result,
-                             transform, args.batch_size,
-                             shuffle=True, num_workers=args.num_workers)
-
-    # Build the models
-    encoder = EncoderCNN(args.embed_size)
-    # the layout encoder hidden state size must be the same with decoder input size
+    # Build Models
+    # encoder = EncoderCNN(args.embed_size)
+    # encoder.eval()  # evaluation mode (BN uses moving mean/variance)
     layout_encoder = LayoutEncoder(args.layout_embed_size, args.embed_size, 100, args.num_layers)
-    decoder = DecoderRNN(args.embed_size, args.hidden_size,
+    decoder = DecoderRNN(args.embed_size, args.hidden_size, 
                          len(vocab), args.num_layers)
+    
 
+    # Load the trained model parameters
+    layout_encoder.load_state_dict(torch.load(args.layout_encoder_path))
+    decoder.load_state_dict(torch.load(args.decoder_path))
+
+    
+    # If use gpu
     if torch.cuda.is_available():
-        encoder.cuda()
         layout_encoder.cuda()
         decoder.cuda()
 
-    # Loss and Optimizer
-    criterion = nn.CrossEntropyLoss()
-    params = list(layout_encoder.parameters()) + list(decoder.parameters()) + \
-	     list(encoder.linear.parameters()) + list(encoder.bn.parameters())
-    optimizer = torch.optim.Adam(params, lr=args.learning_rate)
-
-    # Train the Models
-    total_step = len(data_loader)
-    for epoch in range(args.num_epochs):
-        for i, (images, captions, lengths, label_seqs, location_seqs, layout_lengths) in enumerate(data_loader):
-
-            # Set mini-batch dataset
-            images = to_var(images)
-            captions = to_var(captions)
-            targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
-
-            # Forward, Backward and Optimize
-            # decoder.zero_grad()
-            # layout_encoder.zero_grad()
-            # encoder.zero_grad()
-            
-            # Modify This part for using visual features or not
-             
-            # features = encoder(images)
-            layout_encoding = layout_encoder(label_seqs, location_seqs, layout_lengths)
-            # comb_features = features + layout_encoding
-            comb_features = layout_encoding
-
-            outputs = decoder(comb_features, captions, lengths)
-            
-            loss = criterion(outputs, targets)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            # Print log info
-            if i % args.log_step == 0:
-                print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f, Perplexity: %5.4f'
-                      % (epoch, args.num_epochs, i, total_step,
-                         loss.data[0], np.exp(loss.data[0])))
-
-                # Save the models
-            if (i + 1) % args.save_step == 0:
-                torch.save(decoder.state_dict(),
-                           os.path.join(args.model_path,
-                                        'decoder-%d-%d.pkl' % (epoch + 1, i + 1)))
-                torch.save(layout_encoder.state_dict(),
-                           os.path.join(args.model_path,
-                                        'layout_encoding-%d-%d.pkl' % (epoch + 1, i + 1)))
-
-
-    validation(encoder,layout_encoder,decoder, args,vocab,transform,args.batch_size)
+    validation(layout_encoder,decoder, args,vocab,transform,args.batch_size)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='./models/',
-                        help='path for saving trained models')
-    parser.add_argument('--crop_size', type=int, default=224,
-                        help='size for randomly cropping images')
+
+    # parser.add_argument('--encoder_path', type=str, default='./models/encoder-5-3000.pkl',
+    #                     help='path for trained encoder')
+    parser.add_argument('--layout_encoder_path', type=str, default='./models/layout_encoding-1-20000.pkl',
+                        help='path for trained encoder')
+    parser.add_argument('--decoder_path', type=str, default='./models/decoder-1-20000.pkl',
+                        help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl',
                         help='path for vocabulary wrapper')
+    # from train.py
+    parser.add_argument('--batch_size', type=int, default=20)
     parser.add_argument('--image_dir', type=str, default='./data/resized2014',
                         help='directory for resized images')
     parser.add_argument('--image_dir_val', type=str, default='./data/resized2014_val',
@@ -202,26 +148,20 @@ if __name__ == '__main__':
     parser.add_argument('--coco_detection_result_val', type=str,
                         default='./data/annotations/instances_val2014.json',
                         help='path coco object detection result file for valiadation')
-    parser.add_argument('--log_step', type=int, default=10,
-                        help='step size for prining log info')
-    parser.add_argument('--save_step', type=int, default=1000,
-                        help='step size for saving trained models')
 
-    # Model parameters
-    parser.add_argument('--embed_size', type=int, default=256,
+    parser.add_argument('--embed_size', type=int , default=256,
                         help='dimension of word embedding vectors')
     parser.add_argument('--layout_embed_size', type=int, default=256,
                         help='layout encoding size')
-    parser.add_argument('--hidden_size', type=int, default=512,
+    parser.add_argument('--hidden_size', type=int , default=512,
                         help='dimension of lstm hidden states')
-    parser.add_argument('--num_layers', type=int, default=1,
+    parser.add_argument('--num_layers', type=int , default=1 ,
                         help='number of layers in lstm')
 
-    parser.add_argument('--num_epochs', type=int, default=5)
-    parser.add_argument('--batch_size', type=int, default=20)
     parser.add_argument('--num_workers', type=int, default=2)
     parser.add_argument('--learning_rate', type=float, default=0.001)
     parser.add_argument('--seed', type=int, default=123, help='random generator seed')
+
+    
     args = parser.parse_args()
-    print(args)
     main(args)
