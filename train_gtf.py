64b198071e633efe1cb40bc0b1acb4e7aa5f375f
TamakiSakura <tamaki.sakura@hotmail.com>
2018-03-19 13:13:32 -0700

    new set of parameter

--- train_gtf.py
+++ train_gtf.py
@@ -144,13 +144,13 @@ if __name__ == '__main__':
                         help='layout encoding size')
     parser.add_argument('--hidden_size', type=int, default=512,
                         help='dimension of lstm hidden states')
-    parser.add_argument('--num_layers', type=int, default=1,
-                        help='number of layers in lstm')
+    parser.add_argument('--num_layers', type=int, default=5,
+                        help='number of layers in google transformer')
 
     parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=20)
     parser.add_argument('--num_workers', type=int, default=2)
-    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--learning_rate', type=float, default=0.0001)
     parser.add_argument('--seed', type=int, default=123, help='random generator seed')
     args = parser.parse_args()
     print(args)
